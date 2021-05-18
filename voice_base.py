c8cb1bd12be2935d3af8c2ff41e5fe9ead956d53
srinag marni <60212283+20171CSE0680@users.noreply.github.com>
2021-05-18 09:23:10 +0530

    Add files via upload

---
+++ voice_base.py
@@ -0,0 +1,49 @@
+import speech_recognition as sr
+import  pyautogui as p
+# Initialize the recognizer
+r = sr.Recognizer()
+
+# Function to convert text to
+# speech
+
+
+# Loop infinitely for user to
+# speak
+
+while 1:
+
+    # Exception handling to handle
+    # exceptions at the runtime
+    try:
+
+        # use the microphone as source for input.
+        with sr.Microphone() as source2:
+
+            # wait for a second to let the recognizer
+            # adjust the energy threshold based on
+            # the surrounding noise level
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+
+            # listens for the user's input
+            audio2 = r.listen(source2)
+
+            # Using ggogle to recognize audio
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+
+            print("Did you say " + MyText)
+            if MyText=='take screenshot':
+                img = p.screenshot()
+                img.save('test1.png')
+
+            elif (MyText.find('type')!= -1):
+                p.write(MyText[MyText.find('type')+5:])
+
+    except sr.RequestError as e:
+        print("not a command")
+
+    except sr.UnknownValueError:
+        print("not a command")
+#kar lohellorcbipl
+
+#RC B TYPE RCB
